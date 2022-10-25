e24913baec411c1696643724eb351a6b285fe9e7
Стокалюк Никита Алексеевич <19862468@sigma.sbrf.ru>
2022-10-25 09:41:21 +0300

    add linter in requirements, fix lang code

--- statistic_app/settings.py
+++ statistic_app/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = "rus"
+LANGUAGE_CODE = "ru-RUS"
 
 TIME_ZONE = "Europe/Moscow"
 
