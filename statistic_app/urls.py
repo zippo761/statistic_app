f9cab89b8f82c1979af6f0fbcc509ffe26d7f3b2
Stokalyuk Nikita <zippo1618@gmail.com>
2022-10-25 22:38:04 +0300

    start create authorisation: Model of UserProfile, Serrializers, View

--- statistic_app/urls.py
+++ statistic_app/urls.py
@@ -10,12 +10,17 @@ Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.urls import include, path
+    1. Import to include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+
+    # path to djoser and points
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.jwt')),
+    path("api/account/", include("account.urls")),
 ]
