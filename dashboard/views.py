d194e93736e5918e17e95416edb9d9943c0d3803
stokalyuc <stokalyuc@mail.ru>
2022-11-03 09:44:47 -0400

    edit dashboard/view

--- dashboard/views.py
+++ dashboard/views.py
@@ -18,18 +18,31 @@ class ChartData(APIView):
     permission_classes = []
 
     def get(self, request, format=None):
+        """
+        GET api/chart/data return JSON:
+
+                     article_labels: x
+                     article_data: y
+
+                     """
+        # empty dict to take all data and return like response
         dataset = dict()
+        # take from DashboardData all to send in front
         for data in DashboardData.objects.all():
             dataset[data.data_x] = data.data_y
 
+        # sorted dict
         data_set = sorted(dataset.items(), key=lambda x: x[1])
+        # change type from list --> dict again
         data_set = dict(data_set)
 
+        # represent dict data_set like JSON 
         data = {
             "article_labels": data_set.keys(),
             "article_data": data_set.values(),
         }
-
+        
+        # return response
         return Response(data)
 
 
