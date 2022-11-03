884ca3b11fd2d977f9e5a6ff6dd806a1eae75ff2
stokalyuc <stokalyuc@mail.ru>
2022-11-03 09:17:09 -0400

    fix name of periodic task

--- dashboard/tasks.py
+++ dashboard/tasks.py
@@ -5,10 +5,10 @@ from celery.schedules import crontab
 
 
 @app.on_after_configure.connect
-def setup_periodic_tasks(sender, **kwargs):
+def setup_periodic_tasks(update_db, **kwargs):
     
     # Executes every Monday morning at 7:30 a.m.
-    sender.add_periodic_task(
+    update-db.add_periodic_task(
         crontab(hour=7, minute=30, day_of_week=1),
         update_database(),
     )
