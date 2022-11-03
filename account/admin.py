a64de244aa9acfadd9d64d0511f8ba2e3aaad53a
stokalyuc <stokalyuc@mail.ru>
2022-11-03 10:32:26 -0400

    edit admin table and add field photo to user account

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import UserProfile
+from .models import User
+
+
+class AccountAdmin(admin.ModelAdmin):
+    """Admin table setting"""
+    # what we will see in admin table
+    list_display = ['id', 
+                    'user', 
+                    'description',
+                    'location',
+                    'date_joined',
+                    'simple_user']
+
+    # link for name of user account and id      
+    list_display_links = ('id', 'user')
+
+    # field for search in admin
+    search_fields = ['user', 'date_joined']
+
+
+
+admin.site.register(UserProfile, AccountAdmin)
--- account/models.py
+++ account/models.py
@@ -3,18 +3,31 @@ from django.db import models
 
 
 class UserProfile(models.Model):
+    """Class of model for user account"""
     # user base model User, delete CASCADE, revert attr for model "profile"
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
+    user = models.OneToOneField(User, on_delete=models.CASCADE, 
+                                      related_name="profile", 
+                                      verbose_name='Имя пользователя')
     # field for desc,
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(blank=True, null=True, verbose_name='Описание')
     # exmpl city
-    location = models.CharField(max_length=30, blank=True)
+    location = models.CharField(max_length=30, blank=True, verbose_name='Местоположение')
     # date of registy
-    date_joined = models.DateTimeField(auto_now_add=True)
+    date_joined = models.DateTimeField(auto_now_add=True, verbose_name='Дата регистрации')
     # if object updated it takes date of this event
-    updated_on = models.DateTimeField(auto_now=True)
+    updated_on = models.DateTimeField(auto_now=True, verbose_name='Дата обновления')
     # flag, by default all user have flag simple user.
-    simple_user = models.BooleanField(default=True)
+    simple_user = models.BooleanField(default=True, verbose_name='Простой пользователь?')
+    # file for user photo
+    photo = models.ImageField(upload_to=r'photos/%Y/%m/%d')
 
     def __str__(self):
         return self.user.username
+    
+
+    class Meta:
+        # setting admin panel
+        verbose_name = 'Аккаунт'
+        verbose_name_plural = 'Аккаунты'
+        ordering = ['date_joined', 'user']
+
